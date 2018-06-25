17b8965d29e81e609e70b2eacae7aa6c24629ff8
Sixtus Nnamdi Emecheta <38170092+sislinigeria@users.noreply.github.com>
2018-06-25 11:56:18 +0100

    Update designs.js
    Retype Code

--- designs.js
+++ designs.js
@@ -4,39 +4,47 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-  console.log("makeGrid is running")
-  
-  //declare inputs
-  var platform, cells, rows, gridHeight, gridWidth, submitQuery;
-  
-  //declaring the jQuery
-  platform =$('#pixelCanvas');
-  gridHeight =$('#input_height').val();
-  gridWidth =$('#input_width').val();
-  
-  platform.children().remove()
-  
-  for (x=0; x < gridHeight; x++) {
-  platform.append('<tr></tr>');
-  }
-  rows = $('tr');
-  for (y = 0; y < gridWidth; y++) {
-  rows.append('<td></td>');
-  }
-  cells = platform.find('td');
-  
-  //Change color of td when clicked
-  cells.click(function() {
-    //color input
-    console.log("changeColor is running!");
-    var color; color =$("#colorPicker").val();
-    $(this).attr('bgcolor', color);
-  });
+	console.log("makeGrid is running!")
+	
+	// Select size input
+	
+	var canvas, cell, gridHeight, gridWidth, rows;
+	
+	canvas = $('#pixelCanvas');
+	gridHeight = $('#input_height').val();
+	gridWidth = $('#input_width').val();
+	
+	canvas.children().remove()
+	
+	for (x = 0; x < gridHeight; x++) {
+	canvas.append('<tr></tr>');
+	}
+	
+	rows = $('tr');
+	
+	for (y = 0; y < gridWidth; y++) {
+	rows.append('<td></td>');
+	} 
+	
+	cell = canvas.find('td');
+	
+	// When td is clicked by the user, change color of td
+	cell.click(function() {
+		// Select color input
+		console.log("changeColor is running!");
+		var color;
+		color = $("#colorPicker").val();
+		$(this).attr('bgcolor', color);
+	});
+	
 }
-//Defining the size by user with makeGrid
-  submitQuery = $('input[type="submit"]')
-    submitQuery.click(function(event) {
-      event.preventDefault();
-      makeGrid(); 
-    }
-                     );
+
+// When size is submitted by the user, call makeGrid()
+var submitQuery;
+
+submitQuery = $('input[type="submit"]')
+
+submitQuery.click(function(event) {
+  event.preventDefault();
+  makeGrid();
+});
