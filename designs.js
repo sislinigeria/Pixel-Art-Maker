889dcd76e4231ea0041fd70d492000d36a730c7d
Sixtus Nnamdi Emecheta <38170092+sislinigeria@users.noreply.github.com>
2018-06-25 10:38:54 +0100

    Update designs.js
    Setup Size inputs, grid maker, color selector and grid sizes

--- designs.js
+++ designs.js
@@ -4,7 +4,39 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+  console.log("makeGrid is running")
+  
+  //declare inputs
+  var platform, cells, rows, gridHeight, gridWidth, submitQuery;
+  
+  //declaring the jQuery
+  platform =$('#pixel_canvas');
+  gridHeight =$('#input_height').val();
+  gridWidth =$('#input_width').val();
+  
+  platform.children().remove()
+  
+  for (x=0; x < gridHeight; x++) {
+  platform.append('<tr></tr>');
+  }
+  rows = $('tr');
+  for (y = 0; y < gridWidth; y++) {
+  rows.append('<td></td>');
+  }
+  cell = platform.find('td');
+  
+  //Change color of td when clicked
+  cell.click(function() {
+    //color input
+    console.log("changeColor is running!");
+    var color; color =$("#colorPicker").val();
+    $(this).attr('bgcolor', color);
+  });
 }
+//Defining the size by user with makeGrid
+  submitQuery = $('input[type="submit"]')
+    submitQuery.click(function(event) {
+      event.preventDefault();
+      makeGrid(); 
+    }
+                     );
